3269fb8e6df1d6fa89d81619d58abe763f500e1d
Kaushal Kumar Yadav <kaushalyad321@gmail.com>
2023-12-25 17:50:49 +0545

    changed

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
       <nav className=" bg-slate-100 h-20 flex items-center justify-between w-[100%]">
         <div className="flex gap-x-11 items-center ">
           <div className="cursor-pointer py-1 px-2 text-blue-600 hover:underline font-semibold text-lg">
-            <NavLink to="/">Home</NavLink>
+            <NavLink to="">Home</NavLink>
           </div>
           <div className="cursor-pointer py-1 px-2 text-blue-600 hover:underline font-semibold text-lg">
             <NavLink to="/education">Education</NavLink>
