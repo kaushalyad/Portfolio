c589fdf037cb452b0f2a15d7f730952e2cf11523
Kaushal Kumar Yadav <kaushalyad321@gmail.com>
2024-01-02 15:06:29 +0545

    updated

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import Home from "../Home";
 const Navbar = () => {
   const [highLightColor, setHighLightColor] = useState([
@@ -31,97 +31,108 @@ const Navbar = () => {
     <div id="home">
       <div className=" fixed top-0 z-50 bg-slate-200 h-20 flex items-center w-screen mobile:justify-center smallMobile:justify-center largeTablet:justify-between laptop:justify-between laptop:px-5 smallMobile:w-full">
         <div className="flex justify-center items-center w-screen largeTablet:justify-between largeTablet:gap-x-11 laptop:justify-between">
-        <div className="flex gap-x-11 items-center tablet:gap-x-5 largeTablet:gap-x-4  mobile:hidden smallMobile:hidden  ">
-          <div className="cursor-pointer py-1 px-2 text-blue-600 hover:underline font-semibold text-lg ">
-            <NavLink
-              to=""
-              onClick={() => {
-                highLight(0);
-                document.getElementById("home").scrollIntoView({ behavior: "smooth" });
-              }}
-              style={{ color: highLightColor[0] }}
-            >
-              Home
-            </NavLink>
+          <div className="flex gap-x-11 items-center tablet:gap-x-5 largeTablet:gap-x-4  mobile:hidden smallMobile:hidden  ">
+            <div className="cursor-pointer py-1 px-2 text-blue-600 hover:underline font-semibold text-lg ">
+              <NavLink
+                to=""
+                onClick={() => {
+                  highLight(0);
+                  document
+                    .getElementById("home")
+                    .scrollIntoView({ behavior: "smooth" });
+                }}
+                style={{ color: highLightColor[0] }}
+              >
+                Home
+              </NavLink>
+            </div>
+            <div className="cursor-pointer py-1 px-2 text-blue-600 hover:underline font-semibold text-lg">
+              <NavLink
+                to="#education"
+                onClick={() => {
+                  highLight(1);
+                  document
+                    .getElementById("education")
+                    .scrollIntoView({ behavior: "smooth" });
+                }}
+                style={{ color: highLightColor[1] }}
+              >
+                Education
+              </NavLink>
+            </div>
+            <div className="cursor-pointer py-1 px-2 text-blue-600 hover:underline font-semibold text-lg">
+              <NavLink
+                to="#expertise"
+                onClick={() => {
+                  highLight(2);
+                  document
+                    .getElementById("expertise")
+                    .scrollIntoView({ behavior: "smooth" });
+                }}
+                style={{ color: highLightColor[2] }}
+              >
+                Expertise
+              </NavLink>
+            </div>
+            <div className="cursor-pointer py-1 px-2 text-blue-600 hover:underline font-semibold text-lg">
+              <NavLink
+                to="#work"
+                onClick={() => {
+                  highLight(3);
+                  document
+                    .getElementById("work")
+                    .scrollIntoView({ behavior: "smooth" });
+                }}
+                style={{ color: highLightColor[3] }}
+              >
+                Work
+              </NavLink>
+            </div>
+            <div className="cursor-pointer py-1 px-2 text-blue-600 hover:underline font-semibold text-lg">
+              <NavLink
+                to="#profile"
+                onClick={() => {
+                  highLight(4);
+                  document
+                    .getElementById("profile")
+                    .scrollIntoView({ behavior: "smooth" });
+                }}
+                style={{ color: highLightColor[4] }}
+              >
+                Profile
+              </NavLink>
+            </div>
+            <div className="cursor-pointer py-1 px-2 text-blue-600 hover:underline font-semibold text-lg">
+              <NavLink
+                to="#contact"
+                onClick={() => {
+                  highLight(5);
+                  document
+                    .getElementById("contact")
+                    .scrollIntoView({ behavior: "smooth" });
+                }}
+                style={{ color: highLightColor[5] }}
+              >
+                Contact
+              </NavLink>
+            </div>
           </div>
-          <div className="cursor-pointer py-1 px-2 text-blue-600 hover:underline font-semibold text-lg">
-            <NavLink
-              to="#education"
-              onClick={() => {
-                highLight(1);
-                document.getElementById("education").scrollIntoView({ behavior: "smooth" });
-              }}
-              style={{ color: highLightColor[1] }}
-            >
-              Education
-            </NavLink>
-          </div>
-          <div className="cursor-pointer py-1 px-2 text-blue-600 hover:underline font-semibold text-lg">
-            <NavLink
-              to="#expertise"
-              onClick={() => {
-                highLight(2);
-                document.getElementById("expertise").scrollIntoView({ behavior: "smooth" });
-              }}
-              style={{ color: highLightColor[2] }}
-            >
-              Expertise
-            </NavLink>
-          </div>
-          <div className="cursor-pointer py-1 px-2 text-blue-600 hover:underline font-semibold text-lg">
-            <NavLink
-              to="#work"
-              onClick={() => {
-                highLight(3);
-                document.getElementById("work").scrollIntoView({ behavior: "smooth" });
-
-              }}
-              style={{ color: highLightColor[3] }}
-            >
-              Work
-            </NavLink>
-            
-          </div>
-          <div className="cursor-pointer py-1 px-2 text-blue-600 hover:underline font-semibold text-lg">
-            <NavLink
-              to="#profile"
-              onClick={() => {
-                highLight(4);
-                document.getElementById("profile").scrollIntoView({ behavior: "smooth" });
-              }}
-              style={{ color: highLightColor[4] }}
-            >
-              Profile
-            </NavLink>
-          </div>
-          <div className="cursor-pointer py-1 px-2 text-blue-600 hover:underline font-semibold text-lg">
-            <NavLink
-              to="#contact"
-              onClick={() => {
-                highLight(5);
-                document.getElementById("contact").scrollIntoView({ behavior: "smooth" });
-
-              }}
-              style={{ color: highLightColor[5] }}
-            >
-              Contact
-            </NavLink>
-          </div>
-        </div>
-        <div className="flex items-center mobile:justify-center">
-          <div className="mr-40 flex items-center mobile:mr-0 mobile:justify-center largeTablet:justify-between largeTablet:w-[100vw]">
-            <NavLink to="/">
-              <div className=" content font-serif text-violet-700 mobile:w-96 mobile:flex justify-between text-lg">
-                <div className="line mt-5 tablet:mt-5 ml-6 mobile:ml-0">
-                  <div className="pop-outin italic tablet:text-[9px]">Full Stack Developer</div>
+          <div className="flex items-center mobile:justify-center">
+            <div className="mr-40 flex items-center mobile:mr-0 mobile:justify-center largeTablet:justify-between largeTablet:w-[100vw]">
+              <NavLink to="/">
+                <div className=" content font-serif text-violet-700 mobile:w-96 mobile:flex justify-between text-lg">
+                  <div className="line mt-5 tablet:mt-5 ml-6 mobile:ml-0">
+                    <div className="pop-outin italic tablet:text-[9px]">
+                      Full Stack Developer
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </NavLink>
+              </NavLink>
+            </div>
           </div>
-        </div>
         </div>
       </div>
-      <div  className=" bg-black h-40" ></div>
+      <div className=" bg-black h-40"></div>
       <Home />
     </div>
   );
