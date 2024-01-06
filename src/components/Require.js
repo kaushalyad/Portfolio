e717ebe720701d30fdcb7ba20c6d044835f788c7
Kaushal Kumar Yadav <kaushalyad321@gmail.com>
2024-01-06 15:30:00 +0545

    c

--- src/components/Require.js
+++ src/components/Require.js
@@ -2,7 +2,7 @@ import React from "react";
 const Require = () => {
   return (
     <>
-      (SDE Intern)
+      (SDE)
       <a
         href="https://drive.google.com/file/d/1Y02O-aAl8bKj5zFve1Ag2X2OaI5DvTbd/view?usp=sharing"
         className="text-white"
